06d5ec7ab3514a75d9d3bb20b0e25d81cc5bfafa
Uchio Kondo <udzura@udzura.jp>
2023-06-12 21:22:05 +0900

    Read

--- bench3/bench3.go
+++ bench3/bench3.go
@@ -4,13 +4,14 @@ import (
 	"bytes"
 	"context"
 	"crypto/rand"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strconv"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/montanaflynn/stats"
 	"github.com/octu0/bitcaskdb"
 	"github.com/olekukonko/tablewriter"
@@ -36,15 +37,17 @@ func init() {
 
 func main() {
 	elapCh := make(chan time.Duration)
+	elapCh2 := make(chan time.Duration)
 	resData := make([]float64, 0)
+	resData2 := make([]float64, 0)
 	ctx, stop := context.WithCancel(context.Background())
 
 	var err error = nil
 	go func() {
 		if os.Args[1] == "files" {
-			err = benchFiles(elapCh)
+			err = benchFiles(elapCh, elapCh2)
 		} else {
-			err = benchBitcaskdb(elapCh)
+			err = benchBitcaskdb(elapCh, elapCh2)
 		}
 
 		if err != nil {
@@ -58,11 +61,115 @@ func main() {
 		select {
 		case r := <-elapCh:
 			resData = append(resData, float64(r))
+		case r := <-elapCh2:
+			resData2 = append(resData2, float64(r))
 		case <-ctx.Done():
 			loop = false
 		}
 	}
 
+	fmt.Println("--- Put ---")
+	showStat(resData)
+	fmt.Println("--- Get ---")
+	showStat(resData2)
+}
+
+func benchFiles(ch, ch2 chan time.Duration) error {
+	for i := 0; i < N; i++ {
+		dirname := fmt.Sprintf("%04d", i)
+		os.MkdirAll("./data/"+dirname, 0o0755)
+		start := time.Now()
+		f, err := os.Create(filepath.Join("./data", dirname, "dummy.image"))
+		if err != nil {
+			return err
+		}
+		if _, err := f.Write(data); err != nil {
+			return err
+		}
+		//f.Sync()
+		f.Close()
+		ch <- time.Since(start)
+
+		if N > 500 {
+			if (i+1)%(N/10) == 0 {
+				fmt.Printf("%d times write...\n", i+1)
+			}
+		}
+	}
+
+	for i := 0; i < N; i++ {
+		dirname := fmt.Sprintf("%04d", i)
+		os.MkdirAll("./data/"+dirname, 0o0755)
+		data2 := make([]byte, 5*1024)
+		start := time.Now()
+		f, err := os.Open(filepath.Join("./data", dirname, "dummy.image"))
+		if err != nil {
+			return err
+		}
+		if _, err := f.Read(data2); err != nil {
+			return err
+		}
+		f.Close()
+		ch2 <- time.Since(start)
+
+		if N > 500 {
+			if (i+1)%(N/10) == 0 {
+				fmt.Printf("%d times read...\n", i+1)
+			}
+		}
+	}
+	return nil
+}
+
+func benchBitcaskdb(ch, ch2 chan time.Duration) error {
+	db, err := bitcaskdb.Open("./db/", bitcaskdb.WithMaxDatafileSize(1024*1024*1024))
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	for i := 0; i < N; i++ {
+		dirname := fmt.Sprintf("%04d", i)
+		key := filepath.Join("data", dirname, "dummy.image")
+		start := time.Now()
+
+		if err := db.PutBytes([]byte(key), data); err != nil {
+			return err
+		}
+
+		ch <- time.Since(start)
+
+		if N > 500 {
+			if (i+1)%(N/10) == 0 {
+				fmt.Printf("%d times write...\n", i+1)
+			}
+		}
+	}
+
+	devnull := io.Discard
+	for i := 0; i < N; i++ {
+		dirname := fmt.Sprintf("%04d", i)
+		key := filepath.Join("data", dirname, "dummy.image")
+		start := time.Now()
+		r, err := db.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+		if n, _ := io.Copy(devnull, r); n != 5*1024 {
+			return errors.New("cannot read all")
+		}
+		ch2 <- time.Since(start)
+
+		if N > 500 {
+			if (i+1)%(N/10) == 0 {
+				fmt.Printf("%d times read...\n", i+1)
+			}
+		}
+	}
+	return nil
+}
+
+func showStat(resData []float64) {
 	mean, _ := stats.Mean(resData)
 	stddev, _ := stats.StandardDeviation(resData)
 	p50, _ := stats.Percentile(resData, 50)
@@ -93,56 +200,4 @@ func main() {
 
 	tbl.Render()
 	fmt.Printf("%s\n", buf.String())
-}
-
-func benchFiles(ch chan time.Duration) error {
-	for i := 0; i < N; i++ {
-		dirname := uuid.New().String()
-		os.MkdirAll("./data/"+dirname, 0o0755)
-		f, err := os.Create(filepath.Join("./data", dirname, "dummy.image"))
-		if err != nil {
-			return err
-		}
-		start := time.Now()
-		if _, err := f.Write(data); err != nil {
-			return err
-		}
-		ch <- time.Since(start)
-		f.Sync()
-		f.Close()
-
-		if N > 500 {
-			if (i+1)%(N/10) == 0 {
-				fmt.Printf("%d times write...\n", i+1)
-			}
-		}
-	}
-	return nil
-}
-
-func benchBitcaskdb(ch chan time.Duration) error {
-	db, err := bitcaskdb.Open("./db/", bitcaskdb.WithMaxDatafileSize(1024*1024*1024))
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	for i := 0; i < N; i++ {
-		dirname := uuid.New().String()
-		key := filepath.Join("data", dirname, "dummy.image")
-		start := time.Now()
-
-		if err := db.PutBytes([]byte(key), data); err != nil {
-			return err
-		}
-
-		ch <- time.Since(start)
-
-		if N > 500 {
-			if (i+1)%(N/10) == 0 {
-				fmt.Printf("%d times write...\n", i+1)
-			}
-		}
-	}
-	return nil
 }
